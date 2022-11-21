59e8f3dc80078a7a878aa18abf36065aabafe795
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2022-11-20 18:35:02 -0800

    index.js

--- index.js
+++ index.js
@@ -35,3 +35,201 @@ Each of these classes must have a suite of well defined tests in a tests folder.
 All tests must pass.
 See the Getting Started section for further technical requirements.
 */
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+// Questions
+const questionForManager = [
+    {
+        type: 'input',
+        message: 'What is a name of Team manager?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is an id of Team manager?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is an email address of Team manager?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is an office number of Team manager?',
+        name: 'officeNumber',
+    },
+    {
+        type: 'list',
+        message: 'If you want to add a team member, which role do you want to add?',
+        name: 'teamMember',
+        choices: [
+            'Engineer',
+            'Intern',
+            'None'
+        ]
+    }
+]
+
+const questionForEngineer = [
+    {
+        type: 'input',
+        message: 'What is a name of Engineer?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is an id of Engineer?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is an email address of Engineer?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is a github address of Engineer?',
+        name: 'github',
+    },
+    {
+        type: 'list',
+        message: 'If you want to add a team member, which role do you want to add?',
+        name: 'teamMember',
+        choices: [
+            'Engineer',
+            'Intern',
+            'None'
+        ]
+    }
+]
+
+
+const questionForIntern = [
+    {
+        type: 'input',
+        message: 'What is a name of Intern?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is an id of Intern?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is an email address of Intern?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Which school does Intern go?',
+        name: 'school',
+    },
+    {
+        type: 'list',
+        message: 'If you want to add a team member, which role do you want to add?',
+        name: 'teamMember',
+        choices: [
+            'Engineer',
+            'Intern',
+            'None'
+        ]
+    }
+]
+
+// promt
+
+const promptManager = () => {
+    writeInit();
+    inquirer.promt(questionForManager)
+    .then((resolve) => {
+        const {name, id, email, officeNumber} = resolve;
+        const manager = new Manager(name, id, email, officeNumber);
+        inputManager(manager);
+
+        switch(resolve.team) {
+            case 'Engineer':
+                promptEngineer();
+            case 'Intern':
+                promptIntern();
+            case 'None':
+                writeClose();
+                break;
+        }
+
+    })
+};
+
+const promptEngineer = () => {
+    writeInit();
+    inquirer.promt(questionForEngineer)
+    .then((resolve) => {
+        const {name, id, email, github} = resolve;
+        const engineer = new Engineer(name, id, email, github);
+        inputEngineer(engineer);
+
+        switch(resolve.team) {
+            case 'Engineer':
+                promptEngineer();
+            case 'Intern':
+                promptIntern();
+            case 'None':
+                writeClose();
+                break;
+        }
+
+    })
+};
+
+const promptIntern = () => {
+    writeInit();
+    inquirer.promt(questionForIntern)
+    .then((resolve) => {
+        const {name, id, email, school} = resolve;
+        const intern = new Intern(name, id, email, school);
+        inputIntern(intern);
+
+        switch(resolve.team) {
+            case 'Engineer':
+                promptEngineer();
+            case 'Intern':
+                promptIntern();
+            case 'None':
+                writeClose();
+                break;
+        }
+
+    })
+};
+
+// writeFile
+const writeInit = () => {
+    fs.appendFile('./dist/index.html', PageTemplate.generateInit(), (err) => err? console.error(err) : console.log('Create your team chart!'))
+}
+
+const writeClose = (employees) => {
+    fs.appendFile('./dist/index.html', PageTemplate.closingHTML(employees), (err) => err? console.error(err) : console.log('Team chart created'))
+}
+
+const inputManager = (employees) => {
+    fs.appendFile('./dist/index.html', PageTemplate.generateManager(employees), (err) => err? console.error(err) : console.log('Successfully added'))
+}
+
+const inputEngineer = (employees) => {
+    fs.appendFile('./dist/index.html', PageTemplate.generateEngineer(employees), (err) => err? console.error(err) : console.log('Successfully added'))
+}
+
+const inputIntern = (employees) => {
+    fs.appendFile('./dist/index.html', PageTemplate.generateIntern(employees), (err) => err? console.error(err) : console.log('Successfully added'))
+}
+
+
+// run
+promptManager();
